351f73a2c35532ff5d4d0dd0b43a238cd64b929b
patrykkor <patkor9@gmail.com>
2019-04-14 15:34:11 +0200

    Some content removed,some things changed

--- app/src/main/java/com/example/fiszki/AdddingFiszka.kt
+++ app/src/main/java/com/example/fiszki/AdddingFiszka.kt
@@ -2,13 +2,15 @@ package com.example.fiszki
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_adding_fiszka.*
 
-class AdddingFiszka() : AppCompatActivity(){
+class AdddingFiszka : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
+        val name = intent.getStringExtra("name")
+        val fiszka = Gson().fromJson(name,Fiszka::class.java)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adding_fiszka)
-
         saveButton.setOnClickListener {
 
         }
