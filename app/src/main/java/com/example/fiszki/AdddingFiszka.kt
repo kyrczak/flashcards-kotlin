adaec2e2db2c6751a9d4e0d9a45c873426198fc2
patrykkor <patkor9@gmail.com>
2019-04-16 22:36:35 +0200

    Reading and writing in JSON file

--- app/src/main/java/com/example/fiszki/AdddingFiszka.kt
+++ app/src/main/java/com/example/fiszki/AdddingFiszka.kt
@@ -1,19 +1,24 @@
 package com.example.fiszki
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_adding_fiszka.*
+import java.io.File
 import java.io.FileWriter
 
 class AdddingFiszka : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adding_fiszka)
-        var gson = Gson()
+        val context = applicationContext
+        val file = File(context.filesDir,"list.json")
+        textView5.text = file.absolutePath
+        val gson = Gson()
         saveButton.setOnClickListener {
-           // gson.toJson(Fiszka(polishWord.toString(),englishWord.toString()),FileWriter("list.json"))
-
+            gson.toJson(Fiszka(polishWord.toString(),englishWord.toString()), FileWriter(file))
+            this.finish()
         }
     }
 }
--- app/src/main/java/com/example/fiszki/MainActivity.kt
+++ app/src/main/java/com/example/fiszki/MainActivity.kt
@@ -9,7 +9,9 @@ import com.google.gson.reflect.TypeToken
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileWriter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +20,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         addButton.setOnClickListener {
-            startActivity(Intent(this,AdddingFiszka::class.java))
+            startActivity(Intent(this, AdddingFiszka::class.java))
         }
-        // @miloszratajczyk (https://github.com/miloszratajczyk)
-        // Look at the list.json file
-        // Also updated User.kt
         val file = File(context.filesDir,"list.json")
         textView.text = file.absolutePath
-        //val jsonString = file.bufferedReader().use { it.readText() }
-       // val flashcardsType = object : TypeToken<MutableList<Fiszka>>() {}.type
-       // val flashcards = Gson().fromJson<MutableList<Fiszka>>(jsonString, flashcardsType)
+        val jsonString= file.bufferedReader().use { it.readLine() }
+        textView2.text = jsonString
+        val flashcardsType = object : TypeToken<MutableList<Fiszka>>() {}.type
+        val flashcards = Gson().fromJson<MutableList<Fiszka>>(jsonString, flashcardsType)
 
-        // GO HERE TO LEARN MORE: https://www.baeldung.com/kotlin-json-convert-data-class
-        // Good luck
-
-
-        /*friendsList.apply {
+        friendsList.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            //Also here fiszki -> flashcards
             adapter = FriendsAdapter(flashcards)
-        }*/
+        }
 
     }
 }
