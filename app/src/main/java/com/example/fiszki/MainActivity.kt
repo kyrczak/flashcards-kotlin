d653657ab277d3a0d0dabff089f5d3cfb08ecd3b
miloszratajczyk <milosz2002ratajczyk@gmail.com>
2019-04-17 23:30:12 +0200

    Fix json file

--- app/src/main/java/com/example/fiszki/MainActivity.kt
+++ app/src/main/java/com/example/fiszki/MainActivity.kt
@@ -23,16 +23,31 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, AdddingFiszka::class.java))
         }
         val file = File(context.filesDir,"list.json")
+        // To że objekt pliku istnieje nie znaczy, że plik istnieje
+        // to brzmi źle xD
+        if(!file.exists()) {
+            file.createNewFile() //Utwórz plik
+            file.writeText("Hi What's up?") //Zapisz do pliku
+            // Tutaj sobie musisz zapisac do pliku to co chcesz w nim mieć po utworzeniu, bo tak to będzie se pusty
+            // Polecam `file.` Ctrl + Spacebar tam jest jeszcze dużo fajnych rzeczy
+        }
+        else {
+            file.writeText(file.readText() + " I'm real") //Dopisz do pliku
+        }
         textView.text = file.absolutePath
-        val jsonString= file.bufferedReader().use { it.readLine() }
-        textView2.text = jsonString
-        val flashcardsType = object : TypeToken<MutableList<Fiszka>>() {}.type
-        val flashcards = Gson().fromJson<MutableList<Fiszka>>(jsonString, flashcardsType)
+        textView2.text = file.readText() //Czytaj z pliku
 
+        //var jsonString= file.bufferedReader().use { it.readLine() }
+        // ^ Hmm Thats kotlin you can just do: file.redText() xD
+
+        //textView2.text = jsonString
+        //val flashcardsType = object : TypeToken<MutableList<Fiszka>>() {}.type
+        //val flashcards = Gson().fromJson<MutableList<Fiszka>>(jsonString, flashcardsType)
+/*
         friendsList.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = FriendsAdapter(flashcards)
-        }
+        }*/
 
     }
 }
